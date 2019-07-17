62703289539ccbb7f9b134ca8688e600c0a01bbc
bdbaraban <375@holbertonschool.com>
2019-07-16 21:29:03 -0700

    Configure passport + Auth0 with authenticated routes

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { createServer } from 'http';
 import next from 'next';
+import session from 'express-session';
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
+import uid from 'uid-safe';
 import { connectClient, closeClient } from '../db/utils';
 import api from '../api';
+import authRoutes from './authRoutes';
 
 const app = next({
   dev: process.env.NODE_ENV !== 'production'
@@ -22,7 +27,57 @@ connectClient(process.env.MONGODB, (err): void => {
     .then((): void => {
       const server = express();
 
+      // Add session management to Express
+      const sessionConfig = {
+        secret: uid.sync(18),
+        cookie: {
+          maxAge: 86400 * 1000 // 24 hours in milliseconds
+        },
+        resave: false,
+        saveUninitialized: true
+      };
+      server.use(session(sessionConfig));
+
+      // Configure Auth0 strategy
+      const auth0Strategy = new Auth0Strategy.Strategy(
+        {
+          domain: process.env.AUTH0_DOMAIN,
+          clientID: process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET,
+          callbackURL: process.env.AUTH0_CALLBACK_URL
+        },
+        function(_1, _2, _3, profile, done): void {
+          return done(null, profile);
+        }
+      );
+
+      // Configure Passport
+      passport.use(auth0Strategy);
+      passport.serializeUser((user, done): void => done(null, user));
+      passport.deserializeUser((user, done): void => done(null, user));
+
+      // Add Passport
+      server.use(passport.initialize());
+      server.use(passport.session());
+
+      // Add authentication routes
+      server.use(authRoutes);
+
+      // Add NextRX API
       server.use(api);
+
+      const restrictAccess = (
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ): void => {
+        if (!req.isAuthenticated()) {
+          return res.redirect('/login');
+        }
+        next();
+      };
+
+      server.use('/api/athletes/:email', restrictAccess);
 
       server.get(
         '*',
