d678aa10deafb46d4826ed0ce23a72b4f4393215
bdbaraban <375@holbertonschool.com>
2019-07-16 21:33:57 -0700

    Upload building barrel file for collectively exporting components

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+// Components barrel file
+export { default as AthleteAvatar } from './AthleteAvatar';
+export { default as ActivityChart } from './ActivityChart';
+export { default as SidebarMenu } from './SidebarMenu';
+export { default as ThemeToggleButton } from './ThemeToggleButton';
+export { default as Title } from './Title';
