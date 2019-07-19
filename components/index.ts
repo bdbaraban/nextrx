32ad8342b7b5debfca9613578b3c33b62c833469
bdbaraban <375@holbertonschool.com>
2019-07-18 21:18:08 -0700

    Include newly converted TypeScript components in exports

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,11 @@
 // Components barrel file
 export { default as AthleteAvatar } from './AthleteAvatar';
 export { default as ActivityChart } from './ActivityChart';
+export { default as Divider } from './Divider';
+export { default as Emoji } from './Emoji';
+export { default as Footer } from './Footer';
 export { default as SidebarMenu } from './SidebarMenu';
 export { default as ThemeToggleButton } from './ThemeToggleButton';
 export { default as Title } from './Title';
+export { default as WorkoutCalendar } from './WorkoutCalendar';
+export { default as WorkoutCard } from './WorkoutCard';
