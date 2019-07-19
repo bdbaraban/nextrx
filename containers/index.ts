55be4c5049174238a5e83be6e2a110161a701c69
bdbaraban <375@holbertonschool.com>
2019-07-18 21:18:50 -0700

    Include new WorkoutSection component in exports

--- containers/index.ts
+++ containers/index.ts
@@ -3,3 +3,4 @@ export { default as ActivityBox } from './ActivityBox';
 export { default as AppBar } from './AppBar';
 export { default as InfoSection } from './InfoSection';
 export { default as Sidebar } from './Sidebar';
+export { default as WorkoutSection } from './WorkoutSection';
