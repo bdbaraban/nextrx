9811cd7f9eb312aaf72a03c7033b4acf98dfbb91
bdbaraban <375@holbertonschool.com>
2019-07-19 20:49:22 -0700

    Implement login and logout routes for Express server

--- server/authRoutes.ts
+++ server/authRoutes.ts
@@ -3,38 +3,16 @@ import passport from 'passport';
 
 const authRoutes = express.Router();
 
-authRoutes.get(
-  '/login',
-  passport.authenticate('auth0', {
-    scope: 'openid email profile'
-  }),
-  (_, res): void => res.redirect('/athlete')
-);
-
-authRoutes.get('/callback', (req, res, next): void => {
-  passport.authenticate('auth0', (err, user): void => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect('/login');
-    }
-    req.logIn(user, (err): void => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/athlete');
-    });
-  })(req, res, next);
+// Log in athlete
+authRoutes.post('/login', passport.authenticate('local'), (_, res): void => {
+  res.send({ redirectURI: '/athlete' });
 });
 
+// Log out athlete
 authRoutes.get('/logout', (req, res): void => {
-  req.logout();
-
-  const { AUTH0_DOMAIN, AUTH0_CLIENT_ID, BASE_URL } = process.env;
-  res.redirect(
-    `https://${AUTH0_DOMAIN}/logout?client_id=${AUTH0_CLIENT_ID}&returnTo=${BASE_URL}`
-  );
+  req.session.destroy((): void => {
+    res.redirect('/');
+  });
 });
 
 export default authRoutes;
