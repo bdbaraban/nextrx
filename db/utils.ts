e1e6d542bd34c4fdc308fba982831124c6300244
bdbaraban <375@holbertonschool.com>
2019-07-19 21:01:07 -0700

    Utilize MongoDB .env URI directly instead of receiving it as function parameter

--- db/utils.ts
+++ db/utils.ts
@@ -3,19 +3,32 @@ import { Db, MongoClient, MongoError } from 'mongodb';
 let _client: MongoClient;
 let _db: Db;
 
+/**
+ * Instantiate a MongoDB client.
+ * @param callback {(err) => void} - Error callback.
+ */
 export const connectClient = async (
-  uri: string,
   callback: (err: MongoError) => void
 ): Promise<void> => {
-  MongoClient.connect(uri, { useNewUrlParser: true }, (err, client): void => {
-    _client = client;
-    _db = _client.db('nextrx_db');
-    return callback(err);
-  });
+  MongoClient.connect(
+    process.env.MONGODB,
+    { useNewUrlParser: true },
+    (err, client): void => {
+      _client = client;
+      _db = _client.db('nextrx_db');
+      return callback(err);
+    }
+  );
 };
 
+/**
+ * Fetch the MongoDB nextrx_db database.
+ */
 export const getDB = (): Db => _db;
 
+/**
+ * Close the MongoDB client.
+ */
 export const closeClient = (): void => {
   _client.close();
 };
