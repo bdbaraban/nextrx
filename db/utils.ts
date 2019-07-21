0ffd55776f450c127c83af003f5a7457d41a726e
bdbaraban <375@holbertonschool.com>
2019-07-21 12:52:22 -0700

    Split up MongoDB .env URI into multiple variables

--- db/utils.ts
+++ db/utils.ts
@@ -5,17 +5,22 @@ let _db: Db;
 
 /**
  * Instantiate a MongoDB client.
- * @param callback {(err) => void} - Error callback.
+ * @param callback {(err: MongoError) => void} - Callback function.
  */
 export const connectClient = async (
   callback: (err: MongoError) => void
 ): Promise<void> => {
+  const user = process.env.MONGODB_USER;
+  const password = process.env.MONGODB_PASSWORD;
+  const host = process.env.MONGODB_HOST;
+  const database = process.env.MONGODB_DB;
+
   MongoClient.connect(
-    process.env.MONGODB,
+    `mongodb+srv://${user}:${password}@${host}/test?retryWrites=true`,
     { useNewUrlParser: true },
     (err, client): void => {
       _client = client;
-      _db = _client.db('nextrx_db');
+      _db = _client.db(database);
       return callback(err);
     }
   );
