587e7986889a3bb8adb7d7bc69e690ced1b3954d
bdbaraban <375@holbertonschool.com>
2019-07-22 21:06:49 -0700

    Write jsDoc style function annotations

--- store/theme/reducer.ts
+++ store/theme/reducer.ts
@@ -1,11 +1,17 @@
 import { SET_DARK, SET_LIGHT, ThemeState, ThemeActionTypes } from './types';
 import { lightTheme } from '../../lib/themes';
 
+// Initial theme state
 export const initialThemeState: ThemeState = {
   type: 'light',
   object: lightTheme
 };
 
+/**
+ * Reduce an action into a new theme state.
+ * @param state - Current theme state.
+ * @param action - Redux action to reduce.
+ */
 const themeReducer = (
   state: ThemeState = initialThemeState,
   action: ThemeActionTypes
