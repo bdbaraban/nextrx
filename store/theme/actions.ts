5f865438a86e68a699f1840f83cb1f2856e48de2
bdbaraban <375@holbertonschool.com>
2019-07-22 21:02:38 -0700

    Write jsDoc style function annotations

--- store/theme/actions.ts
+++ store/theme/actions.ts
@@ -1,10 +1,16 @@
 import { SET_DARK, SET_LIGHT, ThemeActionTypes } from './types';
 import { darkTheme, lightTheme } from '../../lib/themes';
 
+/**
+ * Set the global theme to light mode.
+ */
 export const setLightTheme = (): ThemeActionTypes => {
   return { type: SET_LIGHT, payload: lightTheme };
 };
 
+/**
+ * Set the global theme to dark mode.
+ */
 export const setDarkTheme = (): ThemeActionTypes => {
   return { type: SET_DARK, payload: darkTheme };
 };
