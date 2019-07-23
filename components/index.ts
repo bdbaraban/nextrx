930ea51510627e9c4b8de2f6ba61996babefa24d
bdbaraban <375@holbertonschool.com>
2019-07-22 21:45:34 -0700

    Include Popper, Logo and ChangePasswordForm in list of exports

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,15 @@
 // Components barrel file
 export { default as AthleteAvatar } from './AthleteAvatar';
 export { default as ActivityChart } from './ActivityChart';
+export { default as ChangePasswordForm } from './ChangePasswordForm';
 export { default as Divider } from './Divider';
 export { default as Emoji } from './Emoji';
 export { default as Footer } from './Footer';
 export { default as LoginForm } from './LoginForm';
+export { default as Logo } from './Logo';
+export { default as Popper } from './Popper';
+export { default as RecordsTable } from './RecordsTable';
+export { default as SettingsForm } from './SettingsForm';
 export { default as SidebarMenu } from './SidebarMenu';
 export { default as SignUpForm } from './SignUpForm';
 export { default as ThemeToggleButton } from './ThemeToggleButton';
