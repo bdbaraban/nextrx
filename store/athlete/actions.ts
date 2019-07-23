e9748049d3d0132845e5e741899938bb6172dacf
bdbaraban <375@holbertonschool.com>
2019-07-22 21:14:07 -0700

    Implement updateAthlete Redux action function to make update calls on server

--- store/athlete/actions.ts
+++ store/athlete/actions.ts
@@ -1,5 +1,43 @@
-import { AthleteActionTypes, LOGOUT_ATHLETE } from './types';
+import { Action, ActionCreator } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import fetch from 'isomorphic-unfetch';
+import {
+  AthleteState,
+  UPDATE_ATHLETE_FAILURE,
+  UPDATE_ATHLETE_SUCCESS
+} from './types';
+import { Athlete } from 'db/types';
 
-export const logoutAthlete = (): AthleteActionTypes => {
-  return { type: LOGOUT_ATHLETE, payload: null };
+/**
+ * Update the athlete.
+ * @param attributes - Athlete attributes to update.
+ */
+export const updateAthlete: ActionCreator<
+  ThunkAction<void, AthleteState, null, Action<string>>
+> = (
+  attributes: Partial<Athlete>
+): ThunkAction<void, AthleteState, null, Action<string>> => async (
+  dispatch
+): Promise<Action<string>> => {
+  const response = await fetch('/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(attributes)
+  });
+
+  const data = await response.json();
+
+  if (!data.success) {
+    return dispatch({
+      type: UPDATE_ATHLETE_FAILURE,
+      payload: data.message
+    });
+  } else {
+    return dispatch({
+      type: UPDATE_ATHLETE_SUCCESS,
+      payload: attributes
+    });
+  }
 };
