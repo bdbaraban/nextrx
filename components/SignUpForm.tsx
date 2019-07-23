381064985c5f376f25a59987dd7d780e745a7ef8
bdbaraban <375@holbertonschool.com>
2019-07-22 21:44:34 -0700

    Implement basic password regex and add note about sharing email

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
-import { Box, Button, Form, FormField } from 'grommet';
+import { Box, Button, Form, FormField, Text } from 'grommet';
+
+interface ErrorState {
+  status: boolean;
+  message: string;
+}
+
+const initialErrorState = {
+  status: false,
+  message: ''
+};
 
 /**
  * Index page sign up form.
  */
 const SignUpForm = (): React.ReactElement => {
+  const [error, setError] = React.useState<ErrorState>(initialErrorState);
+
   // Attempt to sign up with entered credentials
   const handleSubmit = async (event: any): Promise<void> => {
+    const { email, password } = event.value;
+    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+      return setError({
+        status: true,
+        message: 'Min. 8 chars, 1 letter, 1 number.'
+      });
+    }
+    setError(initialErrorState);
     await fetch('/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(event.value)
+      body: JSON.stringify({ email, password })
     })
       .then((res: Response): any => res.json())
       .then((data): void => {
-        if (data.redirectURI) {
+        if (!data.success) {
+          setError({
+            status: true,
+            message: data.message
+          });
+        } else {
           window.location = data.redirectURI;
         }
       });
@@ -24,11 +49,24 @@ const SignUpForm = (): React.ReactElement => {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormField label="Email*" name="email" required={true} />
-      <FormField label="Password*" name="password" required={true} />
-      <FormField label="Affiliate" name="affiliate" />
+      <FormField htmlFor="email" label="Email*" name="email" required={true} />
+      <FormField
+        htmlFor="password"
+        type="password"
+        label="Password*"
+        name="password"
+        required={true}
+        error={error.status && error.message}
+      />
       <Box pad="small">
         <Button label="Register" type="submit" />
+      </Box>
+      <Box pad="small" justify="center" align="center">
+        <Text size="small" textAlign="center">
+          By clicking Register, you agree to
+          <br />
+          share your email with NextRX.
+        </Text>
       </Box>
     </Form>
   );
