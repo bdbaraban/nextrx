6c11c6c7f6cabf0858beb1379c00f31f7456cbf1
bdbaraban <375@holbertonschool.com>
2019-07-22 21:05:43 -0700

    Implement athlete update actions into reducer

--- store/athlete/reducer.ts
+++ store/athlete/reducer.ts
@@ -1,17 +1,36 @@
-import { AthleteActionTypes, AthleteState, LOGOUT_ATHLETE } from './types';
+import {
+  AthleteActionTypes,
+  AthleteState,
+  UPDATE_ATHLETE_FAILURE,
+  UPDATE_ATHLETE_SUCCESS
+} from './types';
 
+// Initial athlete state
 export const initialAthleteState: AthleteState = {
-  profile: null
+  profile: null,
+  error: ''
 };
 
+/**
+ * Reduce an action into a new athlete state.
+ * @param state - Current athlete state.
+ * @param action - Redux action to reduce.
+ */
 const athleteReducer = (
   state: AthleteState = initialAthleteState,
   action: AthleteActionTypes
 ): AthleteState => {
   switch (action.type) {
-    case LOGOUT_ATHLETE:
+    case UPDATE_ATHLETE_SUCCESS:
       return {
-        profile: action.payload
+        profile: Object.assign({}, state.profile, action.payload),
+        error: ''
+      };
+
+    case UPDATE_ATHLETE_FAILURE:
+      return {
+        profile: state.profile,
+        error: action.payload
       };
 
     default:
