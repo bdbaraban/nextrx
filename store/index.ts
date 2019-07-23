c5d28b39fd81d4b96371192fbfb81c7ba49e26ae
bdbaraban <375@holbertonschool.com>
2019-07-22 21:01:38 -0700

    Apply thunk middleware when initializing Redux store

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,17 @@
-import { combineReducers, createStore, DeepPartial, Store } from 'redux';
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+  DeepPartial,
+  Store
+} from 'redux';
+import thunk from 'redux-thunk';
 import athleteReducer, { initialAthleteState } from './athlete/reducer';
 import themeReducer, { initialThemeState } from './theme/reducer';
 import { AthleteState } from './athlete/types';
 import { ThemeState } from './theme/types';
 
+// Configure default Redux state
 const defaultState: DeepPartial<{
   theme: ThemeState;
   athlete: AthleteState;
@@ -12,15 +20,21 @@ const defaultState: DeepPartial<{
   theme: initialThemeState
 };
 
+// Combine athlete and theme reducers
 const rootReducer = combineReducers({
   athlete: athleteReducer,
   theme: themeReducer
 });
 
+// Redux state type
 export type AppState = ReturnType<typeof rootReducer>;
 
+/**
+ * Instantiate the Redux store.
+ * @param initialState - Default Redux state.
+ */
 export const initializeStore = (
   initialState = defaultState
 ): Store<AppState> => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 };
