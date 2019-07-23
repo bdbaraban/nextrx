ccb0fd27dc67a1e8c0ec625bfcf235bc6a6d8418
bdbaraban <375@holbertonschool.com>
2019-07-22 21:32:50 -0700

    Include RecordsSection in list of exports

--- containers/index.ts
+++ containers/index.ts
@@ -2,5 +2,6 @@
 export { default as ActivityBox } from './ActivityBox';
 export { default as AppBar } from './AppBar';
 export { default as InfoSection } from './InfoSection';
+export { default as RecordsSection } from './RecordsSection';
 export { default as Sidebar } from './Sidebar';
 export { default as WorkoutSection } from './WorkoutSection';
