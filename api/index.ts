c17dd2e2add03b361dfcc56e1a90b8b59371d7e5
bdbaraban <375@holbertonschool.com>
2019-08-05 19:14:37 -0700

    Add new /lifts and /workouts endpoints to express router

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 import athletes from './athletes';
+import lifts from './lifts';
+import workouts from './workouts';
 
 // Instantiate and configure API Express router
 const api = Router();
@@ -13,5 +15,11 @@ api.get('/api/status', (_, res): void => {
 
 // Add athletes routes
 api.use(athletes);
+
+// Add lifts routes
+api.use(lifts);
+
+// Add workouts routes
+api.use(workouts);
 
 export default api;
