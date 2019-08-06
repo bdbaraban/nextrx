7d8ace8c468127214e3c6c6772d01841b88398c5
bdbaraban <375@holbertonschool.com>
2019-08-05 19:14:07 -0700

    Define additional PUT, POST and DELETE endpoints

--- api/athletes.ts
+++ api/athletes.ts
@@ -5,28 +5,65 @@ import ensureAuthenticated from './ensureAuthenticated';
 
 const athletes = Router();
 
-// Fetch an athlete with a given ID.
-athletes.get('/api/athletes/:id', ensureAuthenticated, (req, res): void => {
+// Fetch all athletes.
+athletes.get('/api/athletes', (_, res): void => {
   getDB()
     .collection('athletes')
-    .aggregate([
-      { $match: { _id: new ObjectID(req.params.id) } },
-      {
-        $lookup: {
-          from: 'workouts',
-          localField: '_id',
-          foreignField: 'athlete_id',
-          as: 'workouts'
-        }
-      },
-      {
-        $project: {
-          password: false
-        }
-      }
-    ])
-    .toArray((_, docs): void => {
-      res.send(docs[0]);
+    .find({}, { projection: { password: 0 } })
+    .toArray((err, docs): void => {
+      if (err) throw err;
+      res.send(docs);
+    });
+});
+
+// Fetch an athlete with a given ID.
+athletes.get('/api/athletes/:id', (req, res): void => {
+  const id = req.params.id;
+
+  getDB()
+    .collection('athletes')
+    .findOne({ _id: new ObjectID(id) }, (err, result): void => {
+      if (err) throw err;
+      delete result.password;
+      res.send(result);
+    });
+});
+
+// Fetch all workouts for an athlete with a given ID.
+athletes.get('/api/athletes/:id/workouts', (req, res): void => {
+  const id = req.params.id;
+
+  getDB()
+    .collection('workouts')
+    .find({ athlete_id: new ObjectID(id) })
+    .toArray((err, docs): void => {
+      if (err) throw err;
+      res.send(docs);
+    });
+});
+
+// Update an athlete.
+athletes.put('/api/athletes/:id', ensureAuthenticated, (req, res): void => {
+  const id = req.params.id;
+  const newValues = { $set: req.body };
+
+  getDB()
+    .collection('athletes')
+    .updateOne({ _id: new ObjectID(id) }, newValues, (err): void => {
+      if (err) throw err;
+      res.send(id);
+    });
+});
+
+// Delete an athlete with a given ID.
+athletes.delete('/api/athletes/:id', ensureAuthenticated, (req, res): void => {
+  const id = req.params.id;
+
+  getDB()
+    .collection('athletes')
+    .deleteOne({ _id: new ObjectID(id) }, (err, result): void => {
+      if (err) throw err;
+      res.send(result.deletedCount);
     });
 });
 
