cbdd54307e3f995f20e19765607b809616a8c241
bdbaraban <375@holbertonschool.com>
2019-08-11 20:03:14 -0700

    Refactor as HOC that selects and passes down Redux athlete state

--- pages/athlete.tsx
+++ pages/athlete.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { useSelector } from 'react-redux';
 import { Box } from 'grommet';
-import { Divider, Footer } from '../components';
-import {
-  AppBar,
-  InfoSection,
-  WorkoutSection,
-  RecordsSection
-} from '../containers';
 import withGrommet from '../lib/withGrommet';
+import { AppState } from '../store';
+import { Athlete } from '../db/types';
+import { Divider, Footer } from '../components';
+import { AppBar, InfoSection, RecordsSection, WODSection } from '../containers';
 
 /**
  * Athlete profile page
  */
 const AthletePage: NextPage<{}> = (): React.ReactElement => {
+  // Select current logged in athlete from Redux store
+  const athlete = useSelector(
+    (state: AppState): Athlete => state.athlete.profile
+  );
+
   return (
     <>
       <AppBar />
       <Box flex direction="column" align="center" justify="center">
-        <InfoSection />
-        <Divider text="WOD's" symbol="💪" label="muscle" />
-        <WorkoutSection />
+        <InfoSection athlete={athlete} />
+        <Divider text="WOD's" symbol="🏋️" label="person-lifting-weights" />
+        <WODSection athlete={athlete} />
         <Divider text="Records" symbol="🏅" label="medal" />
-        <RecordsSection />
+        <RecordsSection athlete={athlete} />
       </Box>
       <Footer />
     </>
