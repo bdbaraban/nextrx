5bf60e082ebd94f2a151002ddfd798d13bd70842
bdbaraban <375@holbertonschool.com>
2019-08-11 20:05:11 -0700

    Receive athlete as passed in prop

--- containers/RecordsSection.tsx
+++ containers/RecordsSection.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Box } from 'grommet';
 import { RecordsTable } from '../components';
+import { Athlete } from '../db/types';
 
-const RecordsSection = (): React.ReactElement => {
+/**
+ * RecordsSection container prop types
+ */
+interface RecordsSectionProps {
+  athlete: Athlete;
+}
+
+/**
+ * Section wrapping weightlifting benchmarks table
+ */
+const RecordsSection = ({
+  athlete
+}: RecordsSectionProps): React.ReactElement => {
   return (
     <Box
       flex
@@ -12,7 +25,7 @@ const RecordsSection = (): React.ReactElement => {
       width="85%"
       style={{ minHeight: 'min-content' }}
     >
-      <RecordsTable />
+      <RecordsTable athlete={athlete} />
     </Box>
   );
 };
